1de6abce29af9b90a804c88a940c9bec2cc634fa
hafsagulzar <hafsagulzar109@gmail.com>
2020-07-10 19:01:05 +0500

    Stopwatch

---
+++ app.js
@@ -0,0 +1,47 @@
+var min=0;
+var sec=0;
+var msec=0;
+
+var minH=document.getElementById("min");
+var secH=document.getElementById("sec");
+var msecH=document.getElementById("msec");
+
+var interval;
+function timer(){
+  msec++;
+  msecH.innerHTML=msec;
+  if(msec>=100)
+  {
+      sec++;
+      secH.innerHTML=sec;
+      msec=0;
+  }
+  else
+  if(sec>=60)
+      {
+          min++;
+          minH.innerHTML=min;
+          sec=0;
+      }
+
+}
+
+function start(){
+interval=setInterval(timer,10);
+document.getElementById("button").disabled = true;
+}
+
+function stop(){
+    clearInterval(interval);
+    document.getElementById("button").disabled = false;
+}
+
+function reset(){
+    min=0;
+    sec=0;
+    msec=0;
+    minH.innerHTML=min;
+    secH.innerHTML=sec;
+    msecH.innerHTML=msec;
+    document.getElementById("button").disabled = false;
+}
